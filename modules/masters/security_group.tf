9b6dfc8a7be7b1cc7beb34fc8873fcf91fbe40bc
Daniel Virassamy <camusdv@gmail.com>
2022-01-10 18:12:37 +0000

    deamonize docker. add security group for port 80 to allow letsencrypt to download certificates

--- modules/masters/security_group.tf
+++ modules/masters/security_group.tf
@@ -13,3 +13,12 @@ resource "openstack_networking_secgroup_rule_v2" "master_https" {
   security_group_id = openstack_networking_secgroup_v2.jenkins-master.id
 }
 
+resource "openstack_networking_secgroup_rule_v2" "master_http" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  port_range_min = 80
+  port_range_max = 80
+  protocol = "tcp"
+  remote_ip_prefix = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.jenkins-master.id
+}
--- nginx-proxy/install.sh
+++ nginx-proxy/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker-compose -f nginx-proxy-letsencrypt.yml up
+sudo docker-compose -f nginx-proxy-letsencrypt.yml up -d
 
-sudo docker-compose -f jenkins.yml up
+sudo docker-compose -f jenkins.yml up -d
 
